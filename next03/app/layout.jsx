46ab39cb28719c98bdf721e98bb6764947050c5e
Manash04 <manash9787@gmail.com>
2024-04-21 08:53:37 +0530

    New file added

---
+++ next03/app/layout.jsx
@@ -0,0 +1,14 @@
+import '@styles/global.css';
+
+export const metadata = {
+    title:"Promtopia",
+    description:"Discover & Share AI Prompts"
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
